a459e0744d670f1042308f62aa9bc75540211e1c
Luis Miguel Giraldo Gonzalez <76192117+gotaluism@users.noreply.github.com>
2024-03-19 22:14:51 -0500

    desplegando_v2

--- Moda_Peluda/settings.py
+++ Moda_Peluda/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-yrw0z(9!65e4m3lyht%@w#@dtrfkssje^!_#=8_#i8j#decifg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.134.212.121']
+ALLOWED_HOSTS = ['34.172.20.240']
 
 
 # Application definition
