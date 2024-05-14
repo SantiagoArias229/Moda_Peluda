56d553ab1ae235801c7ad0eb530465ec10ceeabe
Luis Miguel Giraldo Gonzalez <76192117+gotaluism@users.noreply.github.com>
2024-05-14 13:42:21 -0500

    Despliegue #1 GCP

--- Moda_Peluda/settings.py
+++ Moda_Peluda/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-yrw0z(9!65e4m3lyht%@w#@dtrfkssje^!_#=8_#i8j#decifg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.31.171.9']
 
 
 # Application definition
